e51056adede7a62a77c56978a517194b433f9ad8
a1747701 <a1747701@student.adelaide.edu.au>
2022-07-03 13:35:35 +0930

    fixed app.use

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.arguments(express.static(path.resolve__dirname, "../client/build"));
+app.use(express.static(path.resolve__dirname, "../client/build"));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
