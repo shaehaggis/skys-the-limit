ed53cedf72cacbcbc740c9926d256959bc616efe
a1747701 <a1747701@student.adelaide.edu.au>
2022-12-21 15:22:48 +1030

    fixed some warnings

--- client/src/components/FoodDrinkNav.js
+++ client/src/components/FoodDrinkNav.js
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
  
-const FoodDrinkNav = ({ changeDisplayed }) => {
-  const [active, setActive] = useState("food-button");
-  const [foodColor, setFoodColor] = useState({
-    bgColor: "black",
-    color: "white",
-  });
-  const [drinkColor, setDrinkColor] = useState({
-    bgColor: "white",
-    color: "grey",
-  });
+const FoodDrinkNav = () => {
+  // const [active, setActive] = useState("food-button");
+  // const [foodColor, setFoodColor] = useState({
+  //   bgColor: "black",
+  //   color: "white",
+  // });
+  // const [drinkColor, setDrinkColor] = useState({
+  //   bgColor: "white",
+  //   color: "grey",
+  // });
 
-  const onButtonClick = (e) => {
-    if (e.target.className !== active) {
-      if (foodColor.bgColor === "white") {
-        setFoodColor({
-          bgColor: "black",
-          color: "white",
-        });
-        setDrinkColor({
-          bgColor: "white",
-          color: "grey",
-        });
-      } else {
-        setFoodColor({
-          bgColor: "white",
-          color: "grey",
-        });
-        setDrinkColor({
-          bgColor: "black",
-          color: "white",
-        });
-      }
+  // const onButtonClick = (e) => {
+  //   if (e.target.className !== active) {
+  //     if (foodColor.bgColor === "white") {
+  //       setFoodColor({
+  //         bgColor: "black",
+  //         color: "white",
+  //       });
+  //       setDrinkColor({
+  //         bgColor: "white",
+  //         color: "grey",
+  //       });
+  //     } else {
+  //       setFoodColor({
+  //         bgColor: "white",
+  //         color: "grey",
+  //       });
+  //       setDrinkColor({
+  //         bgColor: "black",
+  //         color: "white",
+  //       });
+  //     }
 
-      setActive(e.target.clasName);
-      changeDisplayed();
-    }
-  };
+  //     setActive(e.target.clasName);
+  //     changeDisplayed();
+  //   }
+  // };
   return (
     <div className="category-nav">
       <Link to="/food">
